0e8a193c14082ff16a1649b9f8d2b69fad90b220
gacaltooyo <107365660+gacaltooyo@users.noreply.github.com>
2022-06-15 19:27:05 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,668 @@
+function result(){
+    let Students = {
+        abukar143: {
+            Name:" Abukar Osman Ibrahim",
+            Window:"80",
+            Word:"30",
+            Excel:"50",
+            Access:"60",
+            ppt:"80",
+            Hardware:"80",
+            Multimedia:"90",
+            Webdesign:"100",
+            Total:700,
+            Average:"90%",
+            Grade:"A+"
+        },
+        Abdalla6499: {
+            Name:" Abdalla Dauud Elmi",
+            Window:"80",
+            Word:"30",
+            Excel:"50",
+            Access:"60",
+            ppt:"80",
+            Hardware:"80",
+            Multimedia:"90",
+            Webdesign:"100",
+            Total:700,
+            Average:"90%",
+            Grade:"A+"
+        },
+        gacaltooyo443: {
+            Name:" Abdalla Ali Gacaltooyo",
+            Window:"80",
+            Word:"30",
+            Excel:"50",
+            Access:"60",
+            ppt:"80",
+            Hardware:"80",
+            Multimedia:"90",
+            Webdesign:"100",
+            Total:700,
+            Average:"90%",
+            Grade:"A+"
+            
+        },
+        //student result
+        // abdirahman48: {
+        //     Name:" Abdirahman Osman Mohamed",
+        //     Window:"82",
+        //     Word:"89",
+        //     Excel:"85",
+        //     Access:"80",
+        //     ppt:"84",
+        //     Hardware:"97",
+        //     Multimedia:"100",
+        //     Webdesign:"100",
+        //     Total:717,
+        //     Average:"89.6%",
+        //     Grade:"B+"
+            
+        // },
+        // dalmar98:{
+        //     Name:" Dalmar Baryare Matan ",
+        //     Window:"82",
+        //     Word:"91",
+        //     Excel:"79",
+        //     Access:"72",
+        //     ppt:"85",
+        //     Hardware:"83",
+        //     Multimedia:"100",
+        //     Webdesign:"100",
+        //     Total:692,
+        //     Average:"86.5%",
+        //     Grade:"B+"
+            
+        // },
+        // sayid40: {
+        //     Name:" Sayid Ali Mohamed",
+        //     Window:"79",
+        //     Word:"83",
+        //     Excel:"79",
+        //     Access:"81",
+        //     ppt:"80",
+        //     Hardware:"97",
+        //     Multimedia:"88",
+        //     Webdesign:"96",
+        //     Total:683,
+        //     Average:"85.4%",
+        //     Grade:"B+"
+            
+        // },
+        ruwaydo41: {
+            Name:" Ruwayda Dalel Dhore",
+            Window:"80",
+            Word:"80",
+            Excel:"78",
+            Access:"80",
+            ppt:"90",
+            Hardware:"83",
+            Multimedia:"96",
+            Webdesign:"94",
+            Total:681,
+            Average:"85.1%",
+            Grade:"B+"
+            
+        },
+        sumayo03: {
+            Name:" Sumayo Mudey Ali",
+            Window:"79",
+            Word:"84",
+            Excel:"79",
+            Access:"80",
+            ppt:"85",
+            Hardware:"90",
+            Multimedia:"94",
+            Webdesign:"98",
+            Total:689,
+            Average:"86.1%",
+            Grade:"B+"
+            
+        },
+        sabirin74: {
+            Name:" Sabirin  Daleel Dhore",
+            Window:"85",
+            Word:"78",
+            Excel:"78",
+            Access:"80",
+            ppt:"85",
+            Hardware:"87",
+            Multimedia:"96",
+            Webdesign:"94",
+            Total:683,
+            Average:"85.4%",
+            Grade:"B+"
+            
+        },
+        nasra26:{
+            Name:" Nasra Aways Adam",
+            Window:"85",
+            Word:"80",
+            Excel:"78",
+            Access:"79",
+            ppt:"78",
+            Hardware:"75",
+            Multimedia:"88",
+            Webdesign:"80",
+            Total:643,
+            Average:"80.4%",
+            Grade:"B"
+            
+        },
+        // adam83: {
+        //     Name:" Adam Said Hassan",
+        //     Window:"85",
+        //     Word:"79",
+        //     Excel:"80",
+        //     Access:"75",
+        //     ppt:"85",
+        //     Hardware:"94",
+        //     Multimedia:"90",
+        //     Webdesign:"0",
+        //     Total:588,
+        //     Average:"73.5%",
+        //     Grade:"C+"
+            
+        // },
+        abdirizak31: {
+            Name:" Abdirisak Mohamud Mohamed ",
+            Window:"82",
+            Word:"80",
+            Excel:"79",
+            Access:"83",
+            ppt:"75",
+            Hardware:"88",
+            Multimedia:"88",
+            Webdesign:"96",
+            Total:681,
+            Average:"85.1%",
+            Grade:"B+"
+            
+        },
+        mahad42: {
+            Name:"Mahad Abdi Ali",
+            Window:"82",
+            Word:"79",
+            Excel:"76",
+            Access:"80",
+            ppt:"85",
+            Hardware:"75",
+            Multimedia:"88",
+            Webdesign:"0",
+            Total:565,
+            Average:"70.6%",
+            Grade:"C+"  
+        },
+        // abdishakur93: {
+        //     Name: " Abdishakur Ali mohamed",
+        //     Window:"79",
+        //     Word:"80",
+        //     Excel:"79",
+        //     Access:"78",
+        //     ppt:"76",
+        //     Hardware:"87",
+        //     Multimedia:"80",
+        //     Webdesign:"0",
+        //     Total:559,
+        //     Average:"69.9%",
+        //     Grade:"C+"
+            
+        // },
+        anisa38: {
+            Name:" Anisa Mohamed Hussien",
+            Window:"85",
+            Word:"90",
+            Excel:"80",
+            Access:"82",
+            ppt:"90",
+            Hardware:"95",
+            Multimedia:"90",
+            Webdesign:"Maqaadan",
+            Total:612,
+            Average:"87.4%",
+            Grade:"B+"
+        },
+        naima14: {
+            Name:" Naim Adow Ahmed",
+            Window:"80",
+            Word:"80",
+            Excel:"79",
+            Access:"78",
+            ppt:"82",
+            Hardware:"87",
+            Multimedia:"90",
+            Webdesign:"Maqadaan",
+            Total:576,
+            Average:"82.3%",
+            Grade:"B"
+            
+        },
+        qadra02: {
+            Name:" Qadra Ali Haji",
+            Window:"80",
+            Word:"79",
+            Excel:"80",
+            Access:"85",
+            ppt:"82",
+            Hardware:"90",
+            Multimedia:"90",
+            Webdesign:"Maqaadan",
+            Total:586,
+            Average:"83.7%",
+            Grade:"B"
+        },
+        // hassan94: {
+        //     Name:" Hassan Sharif Ali",
+        //     Window:"80",
+        //     Word:"78",
+        //     Excel:"78",
+        //     Access:"0",
+        //     ppt:"85",
+        //     Hardware:"75",
+        //     Multimedia:"80",
+        //     Webdesign:"0",
+        //     Total:477,
+        //     Average:"59.6%",
+        //     Grade:"C"
+            
+        // },
+        nasteha69: {
+            Name:" Nasteha Abdi Ahmed",
+            Window:"80",
+            Word:"84",
+            Excel:"79",
+            Access:"81",
+            ppt:"85",
+            Hardware:"89",
+            Multimedia:"94",
+            Webdesign:"0",
+            Total:592,
+            Average:"74.8%",
+            Grade:"C+"
+            
+        },
+        // abtidon53: {
+        //     Name:" Abdirahman farah Abdidon",
+        //     Window:"80",
+        //     Word:"79",
+        //     Excel:"80",
+        //     Access:"0",
+        //     ppt:"75",
+        //     Hardware:"80",
+        //     Multimedia:"80",
+        //     Webdesign:"Maqaadan",
+        //     Total:477,
+        //     Average:"67.7%",
+        //     Grade:"C"
+            
+        // },
+        // muhsin84: {
+        //     Name:" Muhsin Mohamed Warsame",
+        //     Window:"85",
+        //     Word:"80",
+        //     Excel:"79",
+        //     Access:"84",
+        //     ppt:"88",
+        //     Hardware:"90",
+        //     Multimedia:"95",
+        //     Webdesign:"89",
+        //     Total:690,
+        //     Average:"86.3%",
+        //     Grade:"B+"
+            
+        // },
+        mohamud33: {
+            Name:" Mohamud Mohamed Mohamud",
+            Window:"79",
+            Word:"78",
+            Excel:"80",
+            Access:"0",
+            ppt:"85",
+            Hardware:"74",
+            Multimedia:"80",
+            Webdesign:"90",
+            Total:566,
+            Average:"70.8%",
+            Grade:"C+"
+            
+        },
+        anisa99: {
+            Name:" Anisa Hassan Samatar",
+            Window:"85",
+            Word:"92",
+            Excel:"80",
+            Access:"82",
+            ppt:"92",
+            Hardware:"86",
+            Multimedia:"100",
+            Webdesign:"0",
+            Total:617,
+            Average:"77.1%",
+            Grade:"C+"
+            
+        },
+        // omar47: {
+        //     Name:" Omar Ibrahim hussien",
+        //     Window:"82",
+        //     Word:"80",
+        //     Excel:"80",
+        //     Access:"80",
+        //     ppt:"85",
+        //     Hardware:"95",
+        //     Multimedia:"90",
+        //     Webdesign:"33 + project",
+        //     Total:625,
+        //     Average:"78.1%",
+        //     Grade:"C+"
+            
+        // },
+        // abdikafi68: {
+        //     Name:" Abdikafi Mohamed Ishaq",
+        //     Window:"94",
+        //     Word:"94",
+        //     Excel:"92",
+        //     Access:"94",
+        //     ppt:"96",
+        //     Hardware:"98",
+        //     Multimedia:"100",
+        //     Webdesign:"49 + project",
+        //     Total:717,
+        //     Average:"90%",
+        //     Grade:"A"
+        // },
+        anisa85: {
+            Name:" Anisa Ahmed Mohamed",
+            Window:"85",
+            Word:"82",
+            Excel:"85",
+            Access:"80",
+            ppt:"85",
+            Hardware:"92",
+            Multimedia:"95",
+            Webdesign:"40 + project",
+            Total:644,
+            Average:"80.5%",
+            Grade:"B"
+            
+        },
+        rahma73: {
+            Name:" Rahma Mohamed Yusuf",
+            Window:"80",
+            Word:"79",
+            Excel:"82",
+            Access:"79",
+            ppt:"80",
+            Hardware:"80",
+            Multimedia:"90",
+            Webdesign:"30 + project",
+            Total:600,
+            Average:"75%",
+            Grade:"C+"
+            
+        },
+        // abdalla48: {
+        //     Name:" Abdullahi Mohamed Mohamud",
+        //     Window:"88",
+        //     Word:"87",
+        //     Excel:"79",
+        //     Access:"80",
+        //     ppt:"87",
+        //     Hardware:"82",
+        //     Multimedia:"95",
+        //     Webdesign:"0",
+        //     Total:598,
+        //     Average:"74.8%",
+        //     Grade:"C+"
+            
+        // },
+        // ahmed10: {
+        //     Name:" Ahmed Abukar Hassan",
+        //     Window:"80",
+        //     Word:"83",
+        //     Excel:"79",
+        //     Access:"82",
+        //     ppt:"88",
+        //     Hardware:"90",
+        //     Multimedia:"85",
+        //     Webdesign:"33 + project",
+        //     Total:620,
+        //     Average:"78%",
+        //     Grade:"C+"
+            
+        // },
+        abdimahad10: {
+            Name:" AbdiMahad Abdullahi Mohamed",
+            Window:"80",
+            Word:"82",
+            Excel:"79",
+            Access:"83",
+            ppt:"85",
+            Hardware:"88",
+            Multimedia:"100",
+            Webdesign:"35 + project",
+            Total:632,
+            Average:"79%",
+            Grade:"C+"
+            
+        },
+        // samiir44: {
+        //     Name:" Samiir Warsame Mohamed",
+        //     Window:"85",
+        //     Word:"84",
+        //     Excel:"83",
+        //     Access:"86",
+        //     ppt:"100",
+        //     Hardware:"92",
+        //     Multimedia:"90",
+        //     Webdesign:"45 + project",
+        //     Total:665,
+        //     Average:"83.1%",
+        //     Grade:"B"
+            
+        // },
+        abdikarim32: {
+            Name:" Abdikarim Mohamed  Hassan",
+            Window:"79",
+            Word:"78",
+            Excel:"78",
+            Access:"76",
+            ppt:"85",
+            Hardware:"0",
+            Multimedia:"80",
+            Webdesign:"0",
+            Total:476,
+            Average:"59.5%",
+            Grade:"C"
+            
+        },
+        rahma44: {
+            Name:" Rahma Abdirashid Hassan",
+            Window:"82",
+            Word:"79",
+            Excel:"80",
+            Access:"79",
+            ppt:"88",
+            Hardware:"88",
+            Multimedia:"90",
+            Webdesign:"Maqadan",
+            Total:591,
+            Average:"84.4%",
+            Grade:"B"
+            
+        },
+        // abdifitah24: {
+        //     Name:" Abdifitah Abdullahi Mohamed",
+        //     Window:"94",
+        //     Word:"94",
+        //     Excel:"80",
+        //     Access:"88",
+        //     ppt:"87",
+        //     Hardware:"98",
+        //     Multimedia:"95",
+        //     Webdesign:"38 + project",
+        //     Total:674,
+        //     Average:"84.3%",
+        //     Grade:"B"
+            
+        // },
+        abdisamad45: {
+            Name:" Abdisamad Ali Mohamed",
+            Window:"80",
+            Word:"79",
+            Excel:"78",
+            Access:"83",
+            ppt:"70",
+            Hardware:"80",
+            Multimedia:"80",
+            Webdesign:"0",
+            Total:550,
+            Average:"68.8%",
+            Grade:"C"
+            
+        },
+        abdisalam01: {
+            Name:" Abdisalam Ali Mohamed",
+            Window:"79",
+            Word:"78",
+            Excel:"80",
+            Access:"79",
+            ppt:"80",
+            Hardware:"75",
+            Multimedia:"73",
+            Webdesign:"Maqadan",
+            Total:544,
+            Average:"77.7%",
+            Grade:"C"
+            
+        },
+        hayat17: {
+            Name:" Hayat yusuf Abdinur",
+            Window:"80",
+            Word:"81",
+            Excel:"79",
+            Access:"82",
+            ppt:"85",
+            Hardware:"85",
+            Multimedia:"95",
+            Webdesign:"0",
+            Total:587,
+            Average:"73.4%",
+            Grade:"C+"
+            
+        },
+        maryamo95: {
+            Name:" Maryamo Abdirisak Mohamed",
+            Window:"80",
+            Word:"86",
+            Excel:"88",
+            Access:"85",
+            ppt:"86",
+            Hardware:"95",
+            Multimedia:"95",
+            Webdesign:"44 + project",
+            Total:659,
+            Average:"82.4%",
+            Grade:"B"
+        },
+        // abdirahman87: {
+        //     Name:" Abdirahman Mohamud Nur ",
+        //     Window:"80",
+        //     Word:"86",
+        //     Excel:"0",
+        //     Access:"0",
+        //     ppt:"85",
+        //     Hardware:"90",
+        //     Multimedia:"100",
+        //     Webdesign:"0",
+        //     Total:441,
+        //     Average:"55.1%",
+        //     Grade:"C"
+            
+        // },
+        asma67: {
+            Name:" Asma Mohamed Abdi",
+            Window:"80",
+            Word:"79",
+            Excel:"80",
+            Access:"78",
+            ppt:"85",
+            Hardware:"78",
+            Multimedia:"95",
+            Webdesign:"30 + project",
+            Total:605,
+            Average:"75.6%",
+            Grade:"C+"
+            
+        },
+        ridwan16: {
+            Name:" Ridwan Ahmed Mohamed",
+            Window:"80",
+            Word:"79",
+            Excel:"80",
+            Access:"78",
+            ppt:"85",
+            Hardware:"79",
+            Multimedia:"0",
+            Webdesign:"0",
+            Total:481,
+            Average:"60.1%",
+            Grade:"C"
+            
+        },
+        mahad70: {
+            Name:" Mahad Mohamud Omar",
+            Window:"91",
+            Word:"91",
+            Excel:"80",
+            Access:"88",
+            ppt:"80",
+            Hardware:"89",
+            Multimedia:"100",
+            Webdesign:"42 + project",
+            Total:661,
+            Average:"82.6%",
+            Grade:"B"
+            
+        }
+
+    }
+
+    let USername = document.getElementById("USername").value;
+    let input = USername.toLowerCase();
+    let definition = Students[input];
+    let output = document.getElementById("Output")
+    let btnResult = document.querySelector(".btnResult");
+    let USernme = document.querySelector("#USername")
+    
+    btnResult.style.display = "none";
+    USernme.style.display = "none";
+    
+    if(definition == undefined){
+        output.innerHTML = "<hr>There is no result <hr>"
+    }else{
+        output.innerHTML = `Name:${definition.Name}<hr>  MS-WINDOW:  &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; ${ definition.Window} <hr>  MS-WORD: &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;  &nbsp; &nbsp;${definition.Word}<hr> MS-EXCEL: &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; ${definition.Excel}<hr> MS-ACCESS: &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; ${definition.Access}<hr>MS-POWERPOINT: &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; ${definition.ppt}<hr> HARDWAREA+: &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp;  &nbsp; ${definition.Hardware}<hr> MULTIMEDIA: &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp;  &nbsp; &nbsp;  &nbsp;${definition.Multimedia} <hr> WEB DESIGN: &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp;${definition.Webdesign}<hr> TOTAL: &nbsp; ${definition.Total} &nbsp; &nbsp; &nbsp; &nbsp; AVERAGE: &nbsp;${definition.Average} &nbsp; &nbsp; &nbsp; GRADE: &nbsp;${definition.Grade}`
+
+        
+    }
+
+   
+
+//     btnResult.addEventListener("click", function (e) {
+//             btnResult.style.display = "none";
+
+//     })
+}
+
+
+
+
+
+
+
+
+
+// let btnResult = document.querySelector(".btnResult")
+
+// btnResult.addEventListener("click", function(e) {
+//     alert("Show")
+// })
